4c97317e53359c5c98fb4e4a87b21f6b0c1c7b96
Victor <victoraweb@gmail.com>
2014-02-10 11:18:09 -0500

    Position field deleted.

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -27,16 +27,6 @@ class Product extends AppModel {
  * @var array
  */
 	public $validate = array(
-		'position' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'visible' => array(
 			'boolean' => array(
 				'rule' => array('boolean'),
